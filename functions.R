0f5ce626ad06263e980d603ef81082602bc0bcea
yanndav <yann.collindavid@gmail.com>
2021-05-08 15:44:04 +0200

    adde jacknif v1

--- functions.R
+++ functions.R
@@ -84,6 +84,86 @@ dgp_heteroskedastic <- function(G,N_g=30,
                     "g"=rep(seq(1:G), each=N_g)))
 }
 
+
+
+dgp_homoskedastic_skewed <- function(G,N_g=30,
+                              b_0 = 0, 
+                              b_1=1,
+                              mu,sigma){
+  # FUNCTION DESCRIPTION   - - - - - - - -
+  
+  # This function codes for the data-generating process as defined in Cameron, Gelbach and Miller (2008).
+  # Errors are independent across clusters but correlated within clusters
+  # Errors are normally distributed and homoskedastic.
+  # We assume same number of units N_g per cluster
+  
+  # DRAWING DATA - - - - - - - - - - - - -
+  
+  ## Error part
+  # Correlation of errors within each cluster
+  e_g = as.vector(replicate(n = G,rnorm(n=N_g, mean=0, sd=1)))
+  # Individual part of error
+  e_ig = rnorm(n=N_g*G, mean=0, sd=1)
+  
+  ## Covariates part 
+  # Correlation of covariates within each cluster
+  z_g = as.vector(replicate(n = G,rnorm(n=N_g, mean=0, sd=1)))
+  # Individual part of covariates
+  z_ig = rnorm(n=N_g*G, mean=0, sd=1)
+  
+  # GENERATING OUTCOME VARIABLES - - - - -
+  
+  u_ig = e_g + e_ig
+  x_ig = exp(mu + sigma(z_g + z_ig))
+  y_ig = b_0 + b_1*x_ig + u_ig
+  
+  # OUTPUT - - - - - - - - - - - - - - - -
+  
+  return(data.frame("y"=y_ig,
+                    "x"=x_ig,
+                    "g"=rep(seq(1:G), each=N_g)))
+  
+}
+
+dgp_heteroskedastic_skewed <- function(G,N_g=30,
+                                b_0 = 1, 
+                                b_1=1,
+                                mu,
+                                sigma){
+  # FUNCTION DESCRIPTION   - - - - - - - -
+  
+  # This function codes for the data-generating process as defined in Cameron, Gelbach and Miller (2008).
+  # Errors are independent across clusters but correlated within clusters
+  # Errors are normally distributed and heteroskedastic.
+  # We assume same number of units N_g per cluster
+  
+  # DRAWING DATA - - - - - - - - - - - - -
+  
+  ## Covariates part 
+  # Correlation of covariates within each cluster
+  z_g = as.vector(replicate(n = G,rnorm(n=N_g, mean=0, sd=1)))
+  # Individual part of covariates
+  z_ig = rnorm(n=N_g*G, mean=0, sd=1)
+  
+  ## Error part
+  # Correlation of errors within each cluster
+  e_g = as.vector(replicate(n = G,rnorm(n=N_g, mean=0, sd=1)))
+  # Individual part of error: heteroskedastic
+  e_ig = rnorm(n=N_g*G, mean=0, sd=9 * (z_g + z_ig)^2)
+  
+  # GENERATING OUTCOME VARIABLES - - - - -
+  
+  u_ig = e_g + e_ig
+  x_ig = exp(mu+sigma*(z_g + z_ig))
+  y_ig = b_0 + b_1*x_ig + u_ig
+  
+  # OUTPUT - - - - - - - - - - - - - - - -
+  
+  return(data.frame("y"=y_ig,
+                    "x"=x_ig,
+                    "g"=rep(seq(1:G), each=N_g)))
+}
+
 # 02. CLUSTER-ROBUST ESTIMATORS ----------------------------------------------------------
 betaH = function(data){
   # FUNCTION DESCRIPTION - - - - - -
@@ -110,7 +190,7 @@ varCR <- function(data){
   return(solve(t(x)%*% x) %*% 
     sum(sapply(1:G,function(g){
     x_g = data$x[data$g==g]
-    u_g = data$y[data$g==g] - x_g %*% beta_hat
+    u_g = (G/(G-1))*data$y[data$g==g] - x_g %*% beta_hat
     return(t(x_g)%*%u_g%*%t(u_g)%*%x_g)
   })) %*%  solve(t(x)%*% x) )
 
@@ -122,4 +202,16 @@ waldCR <- function(data,beta_null=1){
            solve(sqrt(varCR(data))))
 }
 
-waldCR(data,1)
+
+varJACK <- function(data){
+  G=max(data$g)
+  
+  return(((G-1)/G)*
+           sum(sapply(1:G, function(g){
+    betahat = betaH(data)
+    beta_ghat = betaH(data[data$g!=g,])
+    return((beta_ghat-betahat)^2)
+  })))
+
+  
+  }
--- to_run.R
+++ to_run.R
@@ -14,14 +14,67 @@ source("functions.R")
 # 01. DATA GENERATING PROCESS ---------------------------------------------
 data_homosk = data =  dgp_homoskedastic(G=150,N_g=100)
 data_heterosk =data =  dgp_heteroskedastic(G=15,N_g=30)
+data_homo_sk = dgp_homoskedastic_skewed(G=10,
+                                              N_g=30,
+                                              mu=0.5,
+                                              sigma=1)
+data_heterosk_sk = dgp_heteroskedastic_skewed(G=10,
+                                              N_g=30,
+                                              mu=0.5,
+                                              sigma=1)
 
+hist(data_homo_sk$x)
 
 # 02. RESULTS REPLICATION -------------------------------------------------
-x_ig = data_heterosk$x
-y_ig = data_heterosk$y
+k=1
 
-H_beta = solve(t(x_ig)%*%x_ig) %*%t(x_ig)%*%y_ig
-lm(y~x-1,data=data_homosk)
+reject_it = c()
+reject_boot_it = c()
 
+for (it in 1:num) {
+  data =  dgp_homoskedastic(G=5,N_g=5)
+  
+    for (b in 1:B) {
+    # Random sample of the data, with replacement
+    index_b <- sample(N_g,N_g,replace=TRUE)
+    data_b <- do.call(rbind,lapply(1:G,function(k){
+      t = data[data$g==k ,]
+      return(t[index_b,])}))
+
+   
+    
+    # Wald 
+    temp = theta_hat - theta_null
+    Wald_t =  t(temp)%*%J1(y_b,x_b,theta_hat)%*%temp # Wald
+    wald_vec = c(wald_vec,Wald_t)
+    
+  }
+  
+  wald_vec <- sort(wald_vec)
+  cv_b <- wald_vec[floor(B*0.95)]
+  
+  if (is.nan(Wald) == 0) {
+    if (Wald > cv_b) {
+      reject_boot_it = c(reject_boot_it,1)
+    }else{
+      reject_boot_it = c(reject_boot_it,0)
+    }
+  }
+}
+
+mean(reject_boot_it)
+mean(reject_it)
+
+
+waldCR(data_b,1)>1.96
+
+varCR(data)
+sqrt(varJACK(data))
+
+reg = lm(y~x-1,data=data)
+library(lmtest)
+library("clubSandwich")
+reg2=coef_test(reg, df = data, vcov = vcovCR(reg, type = "CR3",cluster = data$g))
+reg2$SE
 # 03. EXTENSION WITH SKEWED ESTIMATOR -------------------------------------
 
