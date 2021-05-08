54a91052e0931cd96bdb2e5ef0267959c8435fc7
yanndav <yann.collindavid@gmail.com>
2021-05-08 23:44:34 +0200

    v1.1 bootstrap t-pairs

--- to_run.R
+++ to_run.R
@@ -11,47 +11,35 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 source("functions.R")
 library("clubSandwich")
 
-# 01. DATA GENERATING PROCESS ---------------------------------------------
-data_homosk = data =  dgp_homoskedastic(G=150,N_g=100)
-data_heterosk =data =  dgp_heteroskedastic(G=15,N_g=30)
-data_homo_sk = dgp_homoskedastic_skewed(G=10,
-                                              N_g=30,
-                                              mu=0.5,
-                                              sigma=1)
-data_heterosk_sk = dgp_heteroskedastic_skewed(G=10,
-                                              N_g=30,
-                                              mu=0.5,
-                                              sigma=1)
 
-hist(data_homo_sk$x)
-
-# 02. RESULTS REPLICATION -------------------------------------------------
+# 01. results_hoc REPLICATION -------------------------------------------------
 # Setting parameters
 mCarlo = 100 # Number of Monte Carlo Iteration
 bStrap= 399 # Number of Boostrap iterations
 clusters = c(5,10,15,20,25,30) # Number of clusters to be tested
 thresh = 1.96 # Significance level for rejection rate
 
+# HOMOSKEDACTIC DGP
 # Creating empty list if not yet in the environment
-if(!exists("results")){
-  results = list()
+if(!exists("results_hoc")){
+  results_hoc = list()
 }
 
-# Results to be estimated:
+# results_hoc to be estimated:
 for (G in clusters){
-  # Initialising results storage if empty list
-  if(length(results)==0){
-    results[[paste(G)]][["ols"]] = list("rejection" = c(),
+  # Initialising results_hoc storage if empty list
+  if(length(results_hoc)==0){
+    results_hoc[[paste(G)]][["ols"]] = list("rejection" = c(),
                                         "wald" = c())
-    results[[paste(G)]][["ols_Bt"]] = list("rejection" = c(),
+    results_hoc[[paste(G)]][["ols_Bt"]] = list("rejection" = c(),
                                            "wald" = c())
-    results[[paste(G)]][["crve"]] = list("rejection" = c(),
+    results_hoc[[paste(G)]][["crve"]] = list("rejection" = c(),
                                         "wald" = c())
-    results[[paste(G)]][["crve_Bt"]] = list("rejection" = c(),
+    results_hoc[[paste(G)]][["crve_Bt"]] = list("rejection" = c(),
                                             "wald" = c())
-    results[[paste(G)]][["cr3"]] = list("rejection" = c(),
+    results_hoc[[paste(G)]][["cr3"]] = list("rejection" = c(),
                                         "wald" = c())
-    results[[paste(G)]][["cr3_Bt"]] = list("rejection" = c(),
+    results_hoc[[paste(G)]][["cr3_Bt"]] = list("rejection" = c(),
                                            "wald" = c())
   } # If not empty we want to add more iterations to improve our accuracy !
   
@@ -65,21 +53,21 @@ for (G in clusters){
 
     # Basic ols variance
     wald_OLS = abs(waldOLS(reg))
-    results[[paste(G)]][["ols"]][["wald"]] = c(results[[paste(G)]][["ols"]][["wald"]],
+    results_hoc[[paste(G)]][["ols"]][["wald"]] = c(results_hoc[[paste(G)]][["ols"]][["wald"]],
                                                wald_OLS)
-    results[[paste(G)]][["ols"]][["rejection"]] = c(results[[paste(G)]][["ols"]][["rejection"]],
+    results_hoc[[paste(G)]][["ols"]][["rejection"]] = c(results_hoc[[paste(G)]][["ols"]][["rejection"]],
                                                as.integer(wald_OLS>thresh))
     # CRVE variance estimator
     wald_CRVE = abs(waldCR(data))
-    results[[paste(G)]][["crve"]][["wald"]] = c(results[[paste(G)]][["crve"]][["wald"]],
+    results_hoc[[paste(G)]][["crve"]][["wald"]] = c(results_hoc[[paste(G)]][["crve"]][["wald"]],
                                                 wald_CRVE)
-    results[[paste(G)]][["crve"]][["rejection"]] = c(results[[paste(G)]][["crve"]][["rejection"]],
+    results_hoc[[paste(G)]][["crve"]][["rejection"]] = c(results_hoc[[paste(G)]][["crve"]][["rejection"]],
                                                 as.integer(wald_CRVE>thresh))
     # CR3 variance estimator
     wald_CR3 = abs(waldOLS(coef_test(reg, df = data, vcov = vcovCR(reg, type = "CR3",cluster = data$g))))
-    results[[paste(G)]][["cr3"]][["wald"]] = c(results[[paste(G)]][["cr3"]][["wald"]],
+    results_hoc[[paste(G)]][["cr3"]][["wald"]] = c(results_hoc[[paste(G)]][["cr3"]][["wald"]],
                                                wald_CR3)
-    results[[paste(G)]][["cr3"]][["rejection"]] = c(results[[paste(G)]][["cr3"]][["rejection"]],
+    results_hoc[[paste(G)]][["cr3"]][["rejection"]] = c(results_hoc[[paste(G)]][["cr3"]][["rejection"]],
                                                as.integer(wald_CR3>thresh))
     
   
@@ -117,47 +105,50 @@ for (G in clusters){
     # Ols variance:
     boot_ols <- sort(boot_ols)
     cv_boot_ols <- boot_ols[floor(bStrap*0.95)]
-    results[[paste(G)]][["ols_Bt"]][["wald"]] = c(results[[paste(G)]][["ols_Bt"]][["wald"]],
+    results_hoc[[paste(G)]][["ols_Bt"]][["wald"]] = c(results_hoc[[paste(G)]][["ols_Bt"]][["wald"]],
                                                   cv_boot_ols)
     if (is.nan(wald_OLS) == 0) {
-      results[[paste(G)]][["ols_Bt"]][["rejection"]] = c(results[[paste(G)]][["ols_Bt"]][["rejection"]],
+      results_hoc[[paste(G)]][["ols_Bt"]][["rejection"]] = c(results_hoc[[paste(G)]][["ols_Bt"]][["rejection"]],
                                                     as.integer(wald_OLS>cv_boot_ols))
     }
     
     # CRVE variance:
     boot_cvre <- sort(boot_crve)
     cv_boot_crve <- boot_cvre[floor(bStrap*0.95)]
-    results[[paste(G)]][["crve_Bt"]][["wald"]] = c(results[[paste(G)]][["crve_Bt"]][["wald"]],
+    results_hoc[[paste(G)]][["crve_Bt"]][["wald"]] = c(results_hoc[[paste(G)]][["crve_Bt"]][["wald"]],
                                                   cv_boot_crve)
     if (is.nan(wald_CRVE) == 0) {
-      results[[paste(G)]][["crve_Bt"]][["rejection"]] = c(results[[paste(G)]][["crve_Bt"]][["rejection"]],
+      results_hoc[[paste(G)]][["crve_Bt"]][["rejection"]] = c(results_hoc[[paste(G)]][["crve_Bt"]][["rejection"]],
                                                          as.integer(wald_CRVE>cv_boot_crve))
     }
     
     # CR3 variance:
     boot_cr3 <- sort(boot_cr3)
     cv_boot_cr3 <- boot_cr3[floor(bStrap*0.95)]
-    results[[paste(G)]][["cr3_Bt"]][["wald"]] = c(results[[paste(G)]][["cr3_Bt"]][["wald"]],
+    results_hoc[[paste(G)]][["cr3_Bt"]][["wald"]] = c(results_hoc[[paste(G)]][["cr3_Bt"]][["wald"]],
                                                   cv_boot_cr3)
     if (is.nan(wald_CRVE) == 0) {
-      results[[paste(G)]][["cr3_Bt"]][["rejection"]] = c(results[[paste(G)]][["cr3_Bt"]][["rejection"]],
+      results_hoc[[paste(G)]][["cr3_Bt"]][["rejection"]] = c(results_hoc[[paste(G)]][["cr3_Bt"]][["rejection"]],
                                                           as.integer(wald_CR3>cv_boot_cr3))
     }
     
     
   }
 }
+saveRDS(object = results_hoc, 'results_hoc_pairs_t.RDS')
 
-# Printing results
-boot_results = c('ols','ols_Bt','crve','crve_Bt','cr3_Bt','cr3')
-table_results = do.call(rbind,lapply(clusters, function(c){
-  tempo = sapply(boot_results,function(res){
-    return(mean(results[[paste(k)]][[res]][["rejection"]]))
+# Printing results_hoc
+results_hoc = readRDS('results_hoc_pairs_t.RDS')
+boot_results_hoc = c('ols','ols_Bt','crve','crve_Bt','cr3_Bt','cr3')
+table_results_hoc = do.call(rbind,lapply(clusters, function(c){
+  tempo = sapply(boot_results_hoc,function(res){
+    return(mean(results_hoc[[paste(k)]][[res]][["rejection"]]))
   })
   tempo = t(as.data.frame(tempo))
   return(data.frame("n_cluster"=c,tempo,row.names = NULL))
 }))
-print(table_results)
+print(table_results_hoc)
+
 
 # 03. EXTENSION WITH SKEWED ESTIMATOR -------------------------------------
 
