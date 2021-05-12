1509eb2403bde4e1f9a379d5c0423b2045826910
yanndav <yann.collindavid@gmail.com>
2021-05-12 21:46:06 +0200

    tempo modifs

--- to_run.R
+++ to_run.R
@@ -17,10 +17,9 @@ library("clubSandwich")
 
 # 01. results_hoc REPLICATION -------------------------------------------------
 # Setting parameters
-mCarlo = 100 # Number of Monte Carlo Iteration
+mCarlo = 10 # Number of Monte Carlo Iteration
 bStrap= 399 # Number of Boostrap iterations
 clusters = c(5,10,15,20,25,30) # Number of clusters to be tested
-thresh = 1.96 # Significance level for rejection rate
 N_g = 30 # number of observation per cluster
 beta_null = 1
 # HOMOSKEDACTIC DGP
@@ -34,6 +33,7 @@ estimators_wald = c('ols','ols_Bt','crve','crve_Bt','cr3_Bt','cr3',
 
 # results_hoc to be estimated:
 for (G in clusters){
+  
   # Initialising results_hoc storage if empty list
   if(length(results_hoc[[paste(G)]])==0){
     for(method in estimators_wald){
@@ -47,14 +47,15 @@ for (G in clusters){
   for (it in 1:mCarlo) {# Start of Monte Carlo iteration
     # Random data generation process
     data = dgp_homoskedastic(G=G,N_g=N_g)
+    thresh = qchisq(0.95,df=1) # Significance level for rejection rate
     
     
     # Estimation of regression:
     reg = lm(y~x-1,data=data)
-
+    
     
     # Basic ols variance
-    wald_OLS = abs(waldOLS(reg, beta_null = beta_null))
+    wald_OLS = abs(waldOLS(reg, beta_null = 1))
     results_hoc[[paste(G)]][["ols"]][["wald"]] = c(results_hoc[[paste(G)]][["ols"]][["wald"]],
                                                wald_OLS)
     results_hoc[[paste(G)]][["ols"]][["rejection"]] = c(results_hoc[[paste(G)]][["ols"]][["rejection"]],
@@ -85,21 +86,21 @@ for (G in clusters){
     boot_wild = c()
     
     for (b in 1:bStrap) {
-      # Random sample of the data, with replacement
-      index_b <- sample(N_g,N_g,replace=TRUE)
-      data_b <- do.call(rbind,lapply(1:G,function(k){
-        t = data[data$g==k ,]
-        return(t[index_b,])}))
+      # Random sample of the clusters, with replacement
+      
+      index_b <- sample(G,G,replace=TRUE)
+      data_b <- do.call(rbind,lapply(index_b,function(k){
+        return(data[data$g==k ,])}))
   
       # Estimation of regression:
       reg = lm(y~x-1,data=data_b)
       
       # Basic ols variance
-      boot_ols = c(boot_ols, abs(waldOLS(reg, beta_null = beta_null)))
+      boot_ols = c(boot_ols, abs(waldOLS(reg, beta_null = betaH(data))))
       
       # CRVE variance estimator
       boot_crve = c(boot_crve,
-                    abs(waldCR(data_b, beta_null = beta_null)))
+                    abs(waldCR(data_b, beta_null = betaH(data))))
       
       # CR3 variance estimator
       boot_cr3 = c(boot_cr3,
@@ -107,7 +108,7 @@ for (G in clusters){
                                          vcov = vcovCR(reg, 
                                                        type = "CR3",
                                                        cluster = data_b$g)),
-                       beta_null = beta_null)))
+                       beta_null = betaH(data))))
       
       
       # Residuals boostrap
@@ -127,7 +128,7 @@ for (G in clusters){
       
       data_r2 = data.frame(x=data$x,
                            g=data$g,
-                           y=beta_r*data$x + wild_p * data_b$ur)
+                           y=beta_r*data$x + wild_p * data$ur)
       
       boot_wild = c(boot_wild,
                     abs(waldCR(data_r2, beta_null = beta_null)))
@@ -186,6 +187,7 @@ for (G in clusters){
     
   }
 }
+
 saveRDS(object = results_hoc, 'results_hoc_pairs_t.RDS')
 
 # 03. EXTENSION WITH SKEWED ESTIMATOR -------------------------------------
